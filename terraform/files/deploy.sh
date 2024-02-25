c16de4e8c0e0fc49aff1f41514f36c059b126047
mari <mari@for-study.myguest.virtualbox.org>
2024-02-25 15:32:24 +0300

    Ansible-1

--- terraform/files/deploy.sh
+++ terraform/files/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -e
-APP_DIR=${1:-$HOME}
-sudo apt-get install -y git
-git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
-cd $APP_DIR/reddit
+#download
+sudo cd /home/yc-user
+sudo apt install git -y
+sudo git clone -b monolith https://github.com/express42/reddit.git
+
+#install
+cd reddit
 bundle install
-sudo mv /tmp/puma.service /etc/systemd/system/puma.service
-sudo systemctl start puma
-sudo systemctl enable puma
+
+#start app
+puma -d
+ps aux | grep puma
--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -13,12 +13,25 @@ variable public_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
-variable image_id {
-  description = "Disk image"
+variable private_key_path {
+  # Описание переменной
+  description = "Path to the private key used for ssh access"
 }
+#variable image_id {
+#  description = "Disk image"
+#}
 variable subnet_id{
   description = "Subnet"
 }
 variable service_account_key_file{
   description = "key .json"
+}
+variable token{
+  description = "Token"
+}
+variable app_disk_image {
+description = "Disk image for reddit app"
+}
+variable db_disk_image {
+description = "Disk image for reddit db"
 }
